26887a48d9e42abc1ab077a01250fcc1d0db1e76
yuriykolomiyets <juriy121@gmail.com>
2022-08-25 12:37:43 +0200

    73. Java Configuration Example

--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class PropertyInjectedGreetingService implements IGreetingService{
     @Override
     public String sayGreeting() {
--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class SetterInjectedGreetingService implements IGreetingService{
     @Override
     public String sayGreeting() {
